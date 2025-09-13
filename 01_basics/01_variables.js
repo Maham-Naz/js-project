8c1ea0e01cb44ecb54211a7a4985572e959dcf3b
Maham Naz <mahamnaz137@gmail.com>
2025-09-13 15:19:54 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 119990
+let accountEmail = "maham@google.com"
+let accountPassword = "12345"
+let accountCity = "Narowal"
+ 
+accountEmail = "jutt@google.com"
+accountPassword = "178745"
+accountCity = "Lahore"
+
+// accountId = 23
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
