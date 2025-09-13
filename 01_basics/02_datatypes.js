981087a2dd5a3fbf8a9eeb7eb3232a6132cffc92
Maham Naz <mahamnaz137@gmail.com>
2025-09-13 15:20:14 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+
+let name  = "Maham"
+let age = 18
+let isloggedIn = false
+
+console.log(typeof "Maham")
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof false)
